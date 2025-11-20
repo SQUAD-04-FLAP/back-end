1d4c46f64794aefb82056b9c6e433664a52240f9
GuilhermeD9 <sgsouza.173@gmail.com>
2025-11-20 10:00:32 -0300

    refactor:#68 Corrections in Dtos

--- src/main/java/dev/squad04/projetoFlap/board/dto/tarefa/CriarTarefaDTO.java
+++ src/main/java/dev/squad04/projetoFlap/board/dto/tarefa/CriarTarefaDTO.java
@@ -1,7 +1,11 @@
 package dev.squad04.projetoFlap.board.dto.tarefa;
 
+import java.time.LocalDateTime;
+
 public record CriarTarefaDTO(
         String titulo,
         String descricao,
+        LocalDateTime dtTermino,
+        String prioridade,
         Integer idQuadro,
         Integer idCriador) {}
