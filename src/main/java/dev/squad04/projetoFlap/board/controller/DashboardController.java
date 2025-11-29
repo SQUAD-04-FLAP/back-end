f0edc1b873dee54ea8a0542a6ec4cbd6cc9a50a0
GuilhermeD9 <sgsouza.173@gmail.com>
2025-11-29 11:24:26 -0300

    feat:#71 Endpoint to get data of dashboard

--- src/main/java/dev/squad04/projetoFlap/board/controller/DashboardController.java
+++ src/main/java/dev/squad04/projetoFlap/board/controller/DashboardController.java
@@ -1,9 +1,10 @@
 package dev.squad04.projetoFlap.board.controller;
 
+import dev.squad04.projetoFlap.board.dto.dashboard.DashboardStatusDTO;
 import dev.squad04.projetoFlap.board.dto.tarefa.TarefaResponseDTO;
 import dev.squad04.projetoFlap.board.entity.Tarefa;
 import dev.squad04.projetoFlap.board.mapper.TarefaMapper;
-import dev.squad04.projetoFlap.board.service.TarefaService;
+import dev.squad04.projetoFlap.board.service.DashboardService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +20,11 @@ import java.util.List;
 @Tag(name = "Dashboard", description = "Endpoints para consulta de dados para o dashboard.")
 public class DashboardController {
 
-    private final TarefaService tarefaService;
+    private final DashboardService dashboardService;
     private final TarefaMapper tarefaMapper;
 
-    public DashboardController(TarefaService tarefaService, TarefaMapper tarefaMapper) {
-        this.tarefaService = tarefaService;
+    public DashboardController(DashboardService dashboardService, TarefaMapper tarefaMapper) {
+        this.dashboardService = dashboardService;
         this.tarefaMapper = tarefaMapper;
     }
 
@@ -32,7 +33,16 @@ public class DashboardController {
     @GetMapping("/proximas/{idSetor}")
     public ResponseEntity<List<TarefaResponseDTO>> buscarTarefasProximasAoVencimento(
             @RequestParam(required = false) Integer idSetor) {
-        List<Tarefa> tarefas = tarefaService.buscarTarefasProximasAoVencimento(idSetor);
+        List<Tarefa> tarefas = dashboardService.buscarTarefasProximasAoVencimento(idSetor);
         return ResponseEntity.ok(tarefaMapper.toDTOList(tarefas));
+    }
+
+    @Operation(summary = "Obtém dados do dashboard",
+            description = "Se idSetor for informado, retorna dados da empresa. Se não, retorna dados gerais.")
+    @GetMapping("/{idSetor}")
+    public ResponseEntity<DashboardStatusDTO> gerarEstatisticasDashboard(
+            @RequestParam(required = false) Integer idSetor) {
+        DashboardStatusDTO estatisticas = dashboardService.gerarEstatisticasDashboard(idSetor);
+        return ResponseEntity.ok(estatisticas);
     }
 }
--- src/main/java/dev/squad04/projetoFlap/board/dto/dashboard/DashboardStatusDTO.java
+++ src/main/java/dev/squad04/projetoFlap/board/dto/dashboard/DashboardStatusDTO.java
@@ -1,5 +1,9 @@
 package dev.squad04.projetoFlap.board.dto.dashboard;
 
 public record DashboardStatusDTO(
+        long totalTarefas,
+        long tarefasConcluidas,
+        long tarefasPendentes,
+        long tarefasAtrasadas
 ) {
 }
